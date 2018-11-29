a2c82170a17a3552a37886cfc7de819c2902f3bb
Eve-ning <johnchangqi@hotmail.com>
2018-11-29 23:54:36 +0800

    Added some more results

--- convertJsonCSV.py
+++ convertJsonCSV.py
@@ -1,20 +1,24 @@
 # Converts JSON to CSV
 import json
 import csv
-with open('api_responses/response10.json', encoding='utf-8') as resp:
-    respJson = json.loads(json.load(resp))
-    
-with open('api_responses/response10.csv', 'w', newline='') as respcsv:
+
+with open('api_responses/out.csv', 'w', newline='') as respcsv:
     csv_file = csv.writer(respcsv)
-    for map in respJson:
-        csv_file.writerow([map["approved_date"],
-                           map["beatmap_id"],
-                           map["beatmapset_id"],
-                           map["difficultyrating"],
-                           map["diff_size"],
-                           map["hit_length"],
-                           map["favourite_count"],
-                           map["playcount"],
-                           map["passcount"],
-                           map["artist"],
-                           map["title"]])
+
+    for x in range(0, 11):
+        with open('api_responses/response' + str(x) + '.json', encoding='utf-8') as resp:
+            respJson = json.loads(json.load(resp))
+
+        for map in respJson:
+            csv_file.writerow([map["approved_date"],
+                               map["beatmap_id"],
+                               map["beatmapset_id"],
+                               map["difficultyrating"],
+                               map["diff_size"],
+                               map["hit_length"],
+                               map["favourite_count"],
+                               map["playcount"],
+                               map["passcount"],
+                               map["artist"],
+                               map["title"],
+                               map["version"]])
